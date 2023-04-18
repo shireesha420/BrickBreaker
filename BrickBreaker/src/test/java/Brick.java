10d44dd3566e3a70d6ac55fa9d255e1e7a9ede06
shireesha420 <131200771+shireesha420@users.noreply.github.com>
2023-04-19 00:52:45 +0530

    Add files via upload

---
+++ BrickBreaker/src/test/java/Brick.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+public class Brick {
+
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        GamePlay game = new GamePlay();
+        obj.setBounds(10,10,700,600);
+        obj.setTitle("BRICK BREAKER");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(game);
+    }
+
+}
